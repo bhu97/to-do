ab7f530257e7bb6d4844ace390835c4b349ee55d
bhu97 <bhumika.r.1997@gmail.com>
2026-01-06 04:27:08 +0530

    first commit

---
+++ todo-app/src/TodoForm.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { Button, TextField } from '@mui/material';
+import { Task } from './types';
+
+interface Props {
+  onSubmit: (task: Task) => void;
+  selectedTask: Task | null;
+}
+
+const TodoForm: React.FC<Props> = ({ onSubmit, selectedTask }) => {
+  const [title, setTitle] = useState('');
+  const [dueDate, setDueDate] = useState('');
+
+  useEffect(() => {
+    if (selectedTask) {
+      setTitle(selectedTask.title);
+      setDueDate(selectedTask.dueDate);
+    }
+  }, [selectedTask]);
+
+  const handleSave = () => {
+    if (!title || !dueDate) return;
+
+    onSubmit({
+      id: selectedTask ? selectedTask.id : Date.now(),
+      title,
+      dueDate,
+      completed: selectedTask ? selectedTask.completed : false,
+    });
+
+    setTitle('');
+    setDueDate('');
+  };
+
+  return (
+    <div style={{ marginTop: 20 }}>
+      <TextField
+        label='Task'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        fullWidth
+      />
+    </div>
+  );
+};
+export default TodoForm;
